354ec38f50a5fa0c855c896e76bba29a4ad81ba3
orlane <orlanetariot@gmail.com>
2019-12-02 09:23:21 +0100

    Page information sup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/Categorie/communication','HomeController@index2')->name('test');
