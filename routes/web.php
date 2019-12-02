43a258054b13e06e2c77cf9a62f75d157993cbe6
Enzo Salmon <enzosalmon1@gmail.com>
2019-12-02 09:10:45 +0100

    Create page ficheAssociation and add route /ficheAssociation

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/ficheAssociation', function () {
+    return view('ficheAssociation');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
