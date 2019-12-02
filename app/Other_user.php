5d86d8fe603b254a14ba3a4d686f2440327ce839
SIO <usersio@157-xubuntu-ppe2.localdomain>
2019-12-02 11:59:34 +0100

    ajout de migration et models

---
+++ app/Other_user.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class other_user extends Model
+{
+    protected $fillable = [
+        'id','name','forename','user_id'
+    ];
+
+
+}
