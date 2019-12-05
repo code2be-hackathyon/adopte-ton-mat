4945ed00f27cf6948c5af91d3d596660ba2ddc04
AntoineHerbert <herber24an@lycee-ndduroc.com>
2019-12-05 15:39:43 +0100

    modif detail materiel

--- database/seeds/Sub_categoriesSeeder.php
+++ database/seeds/Sub_categoriesSeeder.php
@@ -16,7 +16,7 @@ class Sub_categoriesSeeder extends Seeder
             ['label'=>'fauteul roulant','category_id'=>4],
             ['label'=>'lit','category_id'=>2],
             ['label'=>'lève personne','category_id'=>3],
-            ['label'=>'douche','category_id'=>3],
+            ['label'=>'douche','category_id'=>5],
             ['label'=>'J\'utilise mes yeux','category_id'=>1],
             ['label'=>'J\'utilise mes mains','category_id'=>1],
         ]);
